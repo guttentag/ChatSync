ee79b98d02a3b84fc36469745442341284c47550
Eran Guttentag <gutte@mail.com>
2018-08-21 17:17:22 +0300

    adding content to message 😏

--- Reali-ChatDemo/SmartObject/Message.swift
+++ Reali-ChatDemo/SmartObject/Message.swift
@@ -1,5 +1,5 @@
 //
-//  message.swift
+//  Message.swift
 //  Reali-ChatDemo
 //
 //  Created by Eran Guttentag on 21/08/2018.
@@ -16,11 +16,13 @@ class Message: NSObject {
     let id: String
     let timestamp: TimeInterval
     let senderId: String
+    let content: String
     
-    init(_ _id: String, timestamp ts: TimeInterval, sender: String) {
+    init(_ _id: String, timestamp ts: TimeInterval, sender: String, content text: String) {
         self.id = _id
         self.timestamp = ts
         self.senderId = sender
+        self.content = text
     }
 }
 
